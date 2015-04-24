b1f752bafaed85e5aa7d8dbef28a9463fc164272
Jade King <dev@jadeking.co.uk>
2015-04-24 19:31:19 +0100

    refactor flash out

--- app/server.rb
+++ app/server.rb
@@ -13,6 +13,7 @@ require_relative 'controllers/sessions'
 require_relative 'controllers/links'
 require_relative 'controllers/tags'
 require_relative 'controllers/users'
+require_relative 'controllers/flash'
 require_relative 'controllers/application'
 
 enable :sessions
@@ -20,12 +21,3 @@ use Rack::Flash
 use Rack::MethodOverride
 set :sessions_secret, 'super secret'
 set :partial_template_engine, :erb
-
-post '/set-flash' do
-  # set flash notice
-  flash[:notice] = 'Thanks for signing up!'
-  # get flash notice
-  flash[:notice]
-  # set flash entry for current request
-  flash.now[:notice] = 'Thanks for signing up!'
-end
